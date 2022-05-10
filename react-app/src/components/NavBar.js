86216e75a91eef80bd91d81472ef363da268b7e5
Jon Moore <jonevanmoore@yahoo.com>
2022-05-10 15:15:38 -0500

    nav bar start

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,35 +2,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+    <nav className='navbar'>
+      <div className='left-side-nav'>
+        <NavLink to='/' exact={true} className='active photonest-nav-label'>photonest</NavLink>
+      </div>
+      <div className='right-side-nav'>
+        home
+        post
+        profile
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
