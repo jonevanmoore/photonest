ff297df0a69d8ac9e7cc49f19da01ffd800705d8
Jon Moore <jonevanmoore@yahoo.com>
2022-05-11 15:44:10 -0500

    images got

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -6,15 +6,19 @@ from app.forms import NewPost, EditPost
 
 post_routes = Blueprint('posts', __name__)
 
+# GET ALL POSTS
 
-@post_routes.route('/<int:post_id>/')
-def get_post(post_id):
-    post = Post.query.get(post_id).to_dict()
-    return jsonify(post)
+
+@post_routes.route('/')
+@login_required
+def all_posts():
+    posts = Post.query.all()
+    return jsonify([post.to_dict() for post in posts])
+
+# CREATE POST
 
 
 @post_routes.route('/', methods=['POST'])
-@login_required
 def create_post():
     form = NewPost()
     form['csrf_token'].data = request.cookies['csrf_token']
@@ -27,7 +31,6 @@ def create_post():
 
         )
 
-        # form.populate_obj(post)
         db.session.add(post)
         db.session.commit()
         return post.to_dict()
--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { allPosts } from "../../store/post"
+
+
 const Home = () => {
+    const dispatch = useDispatch()
+
+    const posts = Object.values(useSelector(state => state.posts))
+    console.log(posts)
+
+    useEffect(() => {
+        dispatch(allPosts())
+    }, [dispatch])
+
     return (
         <div>
             <h1>HOME</h1>
+            {posts.map(post => (
+                <img src={post.post_image} style={{ width: '200px' }} alt='preview' id='image-preview' />
+            ))}
         </div>
     )
 }
