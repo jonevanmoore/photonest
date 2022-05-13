df98997d60088ea80660f5695c7708926a83839b
Jon Moore <jonevanmoore@yahoo.com>
2022-05-13 18:08:26 -0500

    label change

--- react-app/src/components/Post/DisplayPost.js
+++ react-app/src/components/Post/DisplayPost.js
@@ -51,30 +51,32 @@ const DisplayPost = ({ post, comments }) => {
                                 <Link to={`/${user.username}`}>
                                     <span className="username-display">{user.username}</span>
                                 </Link>
-                                <div className="delete-post-div">
-                                    <span onClick={showModalFunc}><i className="fa-solid fa-trash-can"></i></span>
-                                    {showModal && (
-                                        <Modal closeModalFunc={closeModalFunc}>
-                                            <div className="delete-confirmation-div"
-                                                onClick={stopTheProp}
-                                                onMouseDown={stopTheProp}
-                                                style={{ display: 'flex' }}
-                                            >
-                                                <img src={`${post.post_image}`} style={{ border: '1px solid white', maxWidth: '300px' }} />
-                                                <div className='confirmation-action-div' style={{ padding: '10px', borderRadius: '10px' }}>
-                                                    <div>
-                                                        <span>Are you sure you want to delete this post?</span>
-                                                    </div>
-                                                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                                        <button onClick={deletePost} className='delete-btn'>Delete</button>
-                                                        <button onClick={closeModalFunc} className='cancel-btn'>Cancel</button>
+                                {sessionUser.id === post.user_id && (
+                                    <div className="delete-post-div">
+                                        <span onClick={showModalFunc}><i className="fa-solid fa-trash-can"></i></span>
+                                        {showModal && (
+                                            <Modal closeModalFunc={closeModalFunc}>
+                                                <div className="delete-confirmation-div"
+                                                    onClick={stopTheProp}
+                                                    onMouseDown={stopTheProp}
+                                                    style={{ display: 'flex' }}
+                                                >
+                                                    <img src={`${post.post_image}`} style={{ border: '1px solid white', maxWidth: '300px' }} />
+                                                    <div className='confirmation-action-div' style={{ padding: '10px', borderRadius: '10px' }}>
+                                                        <div>
+                                                            <span>Are you sure you want to delete this post?</span>
+                                                        </div>
+                                                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                                            <button onClick={deletePost} className='delete-btn'>Delete</button>
+                                                            <button onClick={closeModalFunc} className='cancel-btn'>Cancel</button>
 
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </Modal>
-                                    )}
-                                </div>
+                                            </Modal>
+                                        )}
+                                    </div>
+                                )}
                             </div>
                         )
                     }
