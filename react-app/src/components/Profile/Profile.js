f007c721fe659c934f1342b20df09dca99b2cf21
Jon Moore <jonevanmoore@yahoo.com>
2022-07-08 11:59:26 -0700

    profile nav bar

--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchAllPosts } from "../../store/post";
 import { fetchUser } from "../../store/user";
-import Modal from "../Modal/Modal";
+import NavBar from "../NavBar";
 import './Profile.css'
 
 const Profile = () => {
@@ -33,76 +33,88 @@ const Profile = () => {
     }, [dispatch])
 
     return (
-        <div className="profile-body">
-            {/* {window.location.reload(true)} */}
-            <div className="info-and-pics-div">
-                <div className="user-info-div" style={{ display: 'flex', justifyContent: 'center' }}>
-                    <div style={{ marginTop: '80px', display: 'flex' }}>
-                        <div className="profile-pic-div" >
-                            <img src={user?.profile_image} className='profile-pic' />
-                        </div>
-                        <div className="profile-pic-info-mobile">
-                            <div className="mob-pic-follow-info">
+        <>
+            <div className="desktop-el">
+                <NavBar />
+            </div>
+            <div className="mobile-el">
+                <nav className='navbar' style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div>
+                        <span to='/' exact={true} className='active photonest-nav-label'>{username}</span>
+                    </div>
+                </nav >
+            </div>
+            <div className="profile-body">
+                {/* {window.location.reload(true)} */}
+                <div className="info-and-pics-div">
+                    <div className="user-info-div" style={{ display: 'flex', justifyContent: 'center' }}>
+                        <div style={{ marginTop: '80px', display: 'flex' }}>
+                            <div className="profile-pic-div" >
                                 <img src={user?.profile_image} className='profile-pic' />
+                            </div>
+                            <div className="profile-pic-info-mobile">
+                                <div className="mob-pic-follow-info">
+                                    <img src={user?.profile_image} className='profile-pic' />
+                                    <div className="follow-info" style={{ display: 'flex', float: 'left' }}>
+                                        <div className="mob-info">
+                                            <span id="num-info">{userPosts?.length}</span>
+                                            <span>posts</span>
+                                        </div>
+                                        <div className="mob-info">
+                                            <span id="num-info">*</span>
+                                            <span>followers</span>
+                                        </div>
+                                        <div className="mob-info">
+                                            <span id="num-info">*</span>
+                                            <span>following</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span className="mobile-el mobile-name">{`${user?.first_name} ${user?.last_name}`}</span>
+                                <span className="mobile-el mobile-bio">{user?.bio}</span>
+                                <div style={{ marginBottom: '10px' }}>
+                                    {sessionUser?.id === user?.id && (
+                                        <Link to='edit_info' className="edit-profile-btn-mobile">Edit Profile</Link>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="profile-info">
+                                <div>
+                                    <span style={{ fontSize: '20px', float: 'left' }}>{user?.username}</span>
+                                    {sessionUser?.id === user?.id && (
+                                        <Link to='edit_info' className="edit-profile-btn">Edit Profile</Link>
+                                    )}
+                                </div>
                                 <div className="follow-info" style={{ display: 'flex', float: 'left' }}>
-                                    <div className="mob-info">
-                                        <span id="num-info">{userPosts?.length}</span>
-                                        <span>posts</span>
-                                    </div>
-                                    <div className="mob-info">
-                                        <span id="num-info">*</span>
-                                        <span>followers</span>
-                                    </div>
-                                    <div className="mob-info">
-                                        <span id="num-info">*</span>
-                                        <span>following</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <span className="mobile-el mobile-name">{`${user?.first_name} ${user?.last_name}`}</span>
-                            <span className="mobile-el mobile-bio">{user?.bio}</span>
-                            <div style={{ marginBottom: '10px' }}>
-                                {sessionUser?.id === user?.id && (
-                                    <Link to='edit_info' className="edit-profile-btn-mobile">Edit Profile</Link>
-                                )}
-                            </div>
-                        </div>
-                        <div className="profile-info">
-                            <div>
-                                <span style={{ fontSize: '20px', float: 'left' }}>{user?.username}</span>
-                                {sessionUser?.id === user?.id && (
-                                    <Link to='edit_info' className="edit-profile-btn">Edit Profile</Link>
-                                )}
-                            </div>
-                            <div className="follow-info" style={{ display: 'flex', float: 'left' }}>
 
-                                <span>{userPosts?.length} posts</span>
-                                <span>* followers</span>
-                                <span>* following</span>
+                                    <span>{userPosts?.length} posts</span>
+                                    <span>* followers</span>
+                                    <span>* following</span>
 
-                            </div>
-                            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                <div>
-                                    <span style={{ fontWeight: 'bold', float: 'left' }} className='desktop-el'>{`${user?.first_name} ${user?.last_name}`}</span>
                                 </div>
-                                <div>
-                                    <span style={{ float: 'left', maxWidth: '30vw', textAlign: 'left' }} className='desktop-el'>{user?.bio}</span>
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    <div>
+                                        <span style={{ fontWeight: 'bold', float: 'left' }} className='desktop-el'>{`${user?.first_name} ${user?.last_name}`}</span>
+                                    </div>
+                                    <div>
+                                        <span style={{ float: 'left', maxWidth: '30vw', textAlign: 'left' }} className='desktop-el'>{user?.bio}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="img-gallery">
-                    {userPosts.map(post => (
-                        <>
-                            <img src={post?.post_image} key={post?.id} className='indie-img-desktop' />
+                    <div className="img-gallery">
+                        {userPosts.map(post => (
+                            <>
+                                <img src={post?.post_image} key={post?.id} className='indie-img-desktop' />
 
-                            <img src={post?.post_image} key={post?.id} className='indie-img-mobile' />
-                        </>
-                    ))}
+                                <img src={post?.post_image} key={post?.id} className='indie-img-mobile' />
+                            </>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
